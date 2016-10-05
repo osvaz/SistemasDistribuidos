5859c051317fcdd47aa77d85294e0d3b94f480c6
Óscar Vázquez Blanco <vazkezblanc@hotmail.com>
2016-10-05 16:10:24 +0200

    Delete Hola.java

--- Hola.java
+++
@@ -1,9 +0,0 @@
-package holaServer;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Hola extends Remote {
-
-	String saluda(String nombre) throws RemoteException;
-}
